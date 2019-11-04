82eefac27045f4934a90aabc61e65434f3ff68f8
tsigemit <gusbirts@student.gu.se>
2019-11-04 17:53:37 +0100

    some info is added to check

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
 	public static void main(String[] args) {
 	System.out.println("Hello World");
+	System.out.println("To Check Track from jenkins");
+	System.out.println("To track from git by SCM");
 	}
 }
